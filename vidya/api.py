c28f855fb3c63b692f207f1b854feb8c61619862
Rushabh Mehta <rmehta@gmail.com>
2016-05-16 14:55:30 +0530

    try full lib

--- vidya/api.py
+++ vidya/api.py
@@ -6,7 +6,7 @@ def get_kernel():
 	global kernel
 	if not kernel:
 		kernel = aiml.Kernel()
-		for basepath, folders, files in os.walk(frappe.get_app_path('vidya', 'aiml', 'support')):
+		for basepath, folders, files in os.walk(frappe.get_app_path('vidya', 'aiml')):
 			for filename in files:
 				if filename.endswith('.aiml'):
 					print 'learning {0}'.format(os.path.join(basepath, filename))
